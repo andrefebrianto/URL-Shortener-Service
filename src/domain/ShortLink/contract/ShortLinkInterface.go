dc7f9d0e506ac81cdf835c8cdee3b53f8d51ad08
Andre Febrianto <andrefebrianto88@gmail.com>
2021-08-20 23:52:05 +0700

    Change shortlink usecase and quer repository interface

--- src/domain/ShortLink/contract/ShortLinkInterface.go
+++ src/domain/ShortLink/contract/ShortLinkInterface.go
@@ -8,7 +8,7 @@ import (
 
 type ShortLinkUsecase interface {
 	GetAll(ctx context.Context) ([]model.ShortLink, error)
-	GetByCode(ctx context.Context, code string) (model.ShortLink, error)
+	GetByCode(ctx context.Context, code string) (*model.ShortLink, error)
 	Create(ctx context.Context, shortlink *model.ShortLink) error
 	UpdateByCode(ctx context.Context, shortlink *model.ShortLink) error
 	DeleteByCode(ctx context.Context, code string) error
@@ -22,5 +22,5 @@ type ShortLinkCommandRepository interface {
 
 type ShortLinkQueryRepository interface {
 	GetAll(ctx context.Context) ([]model.ShortLink, error)
-	GetByCode(ctx context.Context, code string) (model.ShortLink, error)
+	GetByCode(ctx context.Context, code string) (*model.ShortLink, error)
 }
