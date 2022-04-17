545f718640720e74c5ee76313a5f92d0cc78f1dc
Andre Febrianto <andrefebrianto88@gmail.com>
2022-04-17 23:59:45 +0700

    Update database initialization implementation

--- database/cassandra/CassandraClient.go
+++ database/cassandra/CassandraClient.go
@@ -1,29 +1,16 @@
 package cassandra
 
 import (
+	"github.com/andrefebrianto/URL-Shortener-Service/config"
 	"github.com/gocql/gocql"
-	"github.com/spf13/viper"
 )
 
 var cluster *gocql.ClusterConfig
-var config = viper.New()
 
-func init() {
-	config.SetConfigFile(`configs/configs.json`)
-	err := config.ReadInConfig()
-	if err != nil {
-		panic(err)
-	}
-}
-
-func SetupConnection() {
-	hosts := config.GetStringSlice("cassandra.hosts")
-	cluster = gocql.NewCluster(hosts...)
-	cluster.Keyspace = config.GetString("cassandra.keyspace")
-	// cluster.Authenticator = gocql.PasswordAuthenticator{
-	// 	Username: config.GetString("cassandra.user"),
-	// 	Password: config.GetString("cassandra.password"),
-	// }
+func SetupConnection(config config.CassandraConfiguration) {
+	hosts := config.Hosts
+	cluster := gocql.NewCluster(hosts...)
+	cluster.Keyspace = config.Keyspace
 }
 
 //GetConnection ...
